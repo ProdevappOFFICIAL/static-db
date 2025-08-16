704578df9d0e5ac110c497c8b70964d6ded918b1
ProdevappOFFICIAL <ProdevappOFFICIAL@users.noreply.github.com>
2025-08-16 05:25:37 +0000

    fix: Add offline mode support

--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ main();
 // Bug fixed on Sat Aug 16 04:59:21 UTC 2025
 // Bug fixed on Sat Aug 16 05:04:51 UTC 2025
 // Bug fixed on Sat Aug 16 05:11:50 UTC 2025
+// Bug fixed on Sat Aug 16 05:25:37 UTC 2025
